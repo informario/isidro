173d1f350533da980c7520d49cc88507ad132df0
mario <hernanmlagarde@gmail.com>
2025-05-16 01:39:33 -0300

    estilo interfaz personal en progreso

--- app/src/components/personal/Personal.vue
+++ app/src/components/personal/Personal.vue
@@ -1,32 +1,30 @@
 <template>
-  <div class="h-screen border border-2 border-indigo-500">
-    <div class="h-1/20 border border-2 border-indigo-500 flex items-center justify-between">
-      <!-- Botón para mostrar/ocultar Sidebar solo en móviles -->
-      <button
-        class="p-2 m-2 border rounded bg-indigo-500 text-white"
-        @click="sidebarVisible = !sidebarVisible"
-      >
+  <div class="p-2 h-screen bg-gray-100">
+    <div class="mb-2 h-1/20 flex items-center justify-between rounded-xl bg-white shadow-lg">
+      <button class="p-2 m-2 border rounded bg-indigo-500 text-white" @click="sidebarVisible = !sidebarVisible">
         {{ sidebarVisible ? 'Ocultar menú' : 'Mostrar menú' }}
       </button>
       <Topbar/>
     </div>
-    <div class="flex flex-row h-19/20 border border-2 border-indigo-500">
-      <!-- Sidebar: visible según sidebarVisible -->
+    <div class="flex flex-row h-19/20">
       <transition name="slide">
-        <div
-          v-if="sidebarVisible"
-          class="border border-2 border-indigo-500 w-1/2 md:w-1/6 transition-all duration-300"
-        >
-          <Sidebar class="flex flex-column"/>
+        <div v-if="sidebarVisible" class="mr-2 mb-2 w-1/2 md:w-1/6 transition-all duration-300 rounded-xl bg-white shadow-lg">
+          <div class="flex flex-col">
+            <button @click="contentOption = 'Inscribir socio'">Inscribir socio</button>
+            <button @click="contentOption = 'Buscar socio'">Buscar socio</button>
+          </div>
         </div>
       </transition>
-      <div
-        :class="[
-          'border border-2 border-indigo-500 transition-all duration-300',
-          sidebarVisible ? 'w-1/2 md:w-5/6' : 'w-full'
-        ]"
-      >
-        content
+      <div :class="['mb-2 rounded-xl bg-white shadow-lg transition-all duration-300', sidebarVisible ? 'w-1/2 md:w-5/6' : 'w-full']">
+        <div v-if="contentOption === 'Inscribir socio'">
+          <InscripcionSocio/>
+        </div>
+        <div v-else-if="contentOption === 'Buscar socio'">
+          <BuscarSocio/>
+        </div>
+        <div v-else>
+          Not A/B/C
+        </div>
       </div>
     </div>
   </div>
@@ -42,10 +40,10 @@
   import BuscarSocio from "@/components/personal/BuscarSocio.vue";
   import {isauth} from "@/services/auth.js";
   import Topbar from '@/components/personal/Topbar.vue';
-  import Sidebar from '@/components/personal/Sidebar.vue';
   const router = useRouter()
   const route = useRoute();
   const sidebarVisible = ref(true)
+  const contentOption = ref("")
   async function checkauth(){
     await isauth()
       .then(data => {
